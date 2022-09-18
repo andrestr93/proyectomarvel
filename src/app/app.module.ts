869fae3cb262a9c72ad1ce93d921f368cb77061c
andrestr93 <47641927+andrestr93@users.noreply.github.com>
2022-09-18 15:09:49 +0200

    desarrollo cocteleri

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
+import { SectionComponent } from './components/section/section.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    SectionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
